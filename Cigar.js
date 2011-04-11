5271d0159802878d2fbf3f8d3c4c273070834bbf
David LeMieux <lemieuxster@gmail.com>
2011-04-11 09:35:03 -0700

    Added path queue, changed load implementation, better closure, chaining, getting better every day.

--- Cigar.js
+++ Cigar.js
@@ -1,36 +1,86 @@
 //Cigar - it imports files
 (function() {
-    Cigar = {
-        _paths : {}
+    //The Cigar Object
+    Cigar = {};
+
+    //Queue of paths to load
+    var cigar_queue = [],
+
+    //Wait while paths are added to the queue
+    cigar_wait = null,
+
+    //Paths imported
+    cigar_paths = {},
+
+    //Complete function
+    cigar_complete = function(){},
+
+    //Wait for script to load (5 seconds)
+    cigar_timeout = null,
+
+    //Append script tag to head
+    cigar_loadScript = function() {
+        var tag = cigar_queue.shift();
+        var head = document.documentElement.firstChild;
+        if (!head || (head.nodeName && head.nodeName.toLowerCase().indexOf("comment")>-1)) {
+            head = document.getElementsByTagName("head")[0];
+        }
+        head.appendChild(tag);
     };
 
-    Cigar._import = function(importPath, config) {
-        if (!Cigar._paths[importPath]) {
-            var scriptPath = importPath.replace(/\./g, "/") + ".js";
+    //Create script tag, add to queue, return Cigar for chaining
+    Cigar._import = function(classPath, config) {
+        if (!cigar_paths[classPath]) {
+            if (cigar_wait) {
+                clearTimeout(cigar_wait);
+            }
+
+            var scriptPath = classPath.replace(/\./g, "/") + ".js";
+            if (config && typeof config.cacheBust !== undefined && config.cacheBust != "") {
+                scriptPath = scriptPath + "?" + config.cacheBust
+            }
 
             var tag = document.createElement("script");
             tag.type = "text/javascript";
             if (config && config.async) {
                 tag.setAttribute('async', 'true');
             }
-
-            if (config && typeof config.cacheBust !== undefined && config.cacheBust != "") {
-                scriptPath = scriptPath + "?" + config.cacheBust
-            }
-
-            //TODO allow setting of "classPath" to determine script path root
             tag.src = "/" + scriptPath;
+            tag.onload = Cigar.next;
+            tag.onreadystatechange = function () {
+                if (this.readyState == 'complete') {
+                    Cigar.next();
+                }
+            };
 
-            //TODO look this up once and remember it
-            var head = document.documentElement.firstChild;
-            if (!head || (head.nodeName && head.nodeName.toLowerCase().indexOf("comment")>-1)) {
-                head = document.getElementsByTagName("head")[0];
-            }
-            head.appendChild(tag);
-            Cigar._paths[importPath] = "1";
+            cigar_paths[classPath] = scriptPath;
+            cigar_queue.push(tag);
         }
+
+        cigar_wait = setTimeout(Cigar.next, 73);
+
+        return Cigar;
+    };
+
+    //Do the next tag in the queue
+    Cigar.next = function() {
+        if (cigar_timeout) {
+            clearTimeout(cigar_timeout);
+        }
+        if (cigar_queue.length > 0) {
+            cigar_loadScript();
+            cigar_timeout = setTimeout(Cigar.next, 5000);
+        } else {
+            cigar_complete();
+        }
+        return Cigar;
+    };
+
+    //Set the complete callback function
+    Cigar.complete = function(callbackFn) {
+        cigar_complete = callbackFn;
+        return Cigar;
     };
 
     _import = Cigar._import;
 })();
-
